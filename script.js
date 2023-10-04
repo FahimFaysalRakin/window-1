ac50bbf5e77ca8693b2266b4fdfc2d31a2074382
FahimFaysalRakin <144623739+FahimFaysalRakin@users.noreply.github.com>
2023-10-04 19:50:43 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+Window.caches
+//logs text to the console.
+console.log("login information");
+
+//outputs an informational message to the Web console.
+console.info("information messge to the web console");
+
+//outputs an error message to the Web console.
+console.error("error message to the web console");
+
+//outputs a warning message to the Web console.
+console.warn("warning message to the Web console");
+
+//window.close("close the window");
+//window.open();
+
+
+history.forward();
+
+//print the page
+window.print();
+
+//display a dialog with an optional message prompting the user to input some text, and to wait until the user either submits the text or cancels the dialog.
+window.prompt("name");
+
+//stops further resource loading in the current browsing context
+//window.stop();
+
+//provides an interface for manipulating the browser session history
+window.history.back(1);
+
+location.hostname;
+console.hostname();
